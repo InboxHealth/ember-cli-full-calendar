fdf75e1eba669feab83feb42e4b251334badca92
Richard Stovall <richard.stovall@inboxhealth.com>
2024-11-18 23:11:45 -0500

    ...

---
+++ .eslintrc.js
@@ -0,0 +1,63 @@
+'use strict';
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  plugins: [
+    'import',
+    'simple-import-sort',
+    'eslint-plugin-ember',
+    'eslint-plugin-node',
+    'eslint-plugin-prettier',
+    'ember',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:ember/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+    'plugin:qunit/recommended',
+  ],
+  env: {
+    browser: true,
+  },
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
+  ],
+};
